b2d0565289573e5e4c162fbe6affedc8dd1bd1a9
ALOK YADAV <alokgamer70047@gmail.com>
2026-01-17 19:25:32 +0530

    Refactor password handling in person schema
    Refactor password hashing and comparison methods for better error handling and clarity.

--- Models/person.js
+++ Models/person.js
@@ -36,21 +36,37 @@ const personSchema = new mongoose.Schema({
   }
 });
 
-//  Hash password before save (CORRECT)
-personSchema.pre('save', async function () {
-  if (!this.isModified('password')) 
-    return next();
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+personSchema.pre('save', async function(next){
+    const person = this;
 
-// Compare password
-personSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
+    // Hash the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();
 
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 //  Prevent OverwriteModelError
 module.exports =
   mongoose.models.person || mongoose.model('person', personSchema);

fc58ae96993bae9516798ae93a890c079c405b06
ALOK YADAV <alokgamer70047@gmail.com>
2026-01-17 19:31:39 +0530

    Simplify password hashing in person model
    Refactor password hashing logic in personSchema pre-save hook.

--- Models/person.js
+++ Models/person.js
@@ -35,38 +35,18 @@ const personSchema = new mongoose.Schema({
     required: true
   }
 });
+personSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-personSchema.pre('save', async function(next){
-    const person = this;
-
-    // Hash the password only if it has been modified (or is new)
-    if(!person.isModified('password')) return next();
-
-    try{
-        // hash password generation
-        const salt = await bcrypt.genSalt(10);
-
-        // hash password
-        const hashedPassword = await bcrypt.hash(person.password, salt);
-        
-        // Override the plain password with the hashed one
-        person.password = hashedPassword;
-        next();
-    }catch(err){
-        return next(err);
-    }
-})
 
 personSchema.methods.comparePassword = async function(candidatePassword){
-    try{
-        // Use bcrypt to compare the provided password with the hashed password
-        const isMatch = await bcrypt.compare(candidatePassword, this.password);
-        return isMatch;
-    }catch(err){
-        throw err;
-    }
-}
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 //  Prevent OverwriteModelError
 module.exports =
   mongoose.models.person || mongoose.model('person', personSchema);
